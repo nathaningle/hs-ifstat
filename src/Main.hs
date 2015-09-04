076692325a04f799a74ab9543feb991772f1c03d
Nathan Ingle <elngi.nahtan@gmail.com>
2015-09-04 23:52:49 +1000

    Add code to report seconds with zero bits

--- src/Main.hs
+++ src/Main.hs
@@ -16,19 +16,35 @@ import Network.Pcap.Conduit
 import Network.Pcap.Conduit.SourceLiveForever
 import Data.Conduit
 import qualified Data.Conduit.List as CL
+import Data.Conduit.Lift (evalStateC)
 import Data.Conduit.EndOnQ
+import Data.Word (Word32)
+import Control.Monad.State
 
 preambleEtc :: Integer
 preambleEtc = 24
 
-printAndReturnPkts :: [Packet] -> IO [Packet]
-printAndReturnPkts ps = putBpsStr >> return ps
+printPacketGroup :: [Packet] -> IO ()
+printPacketGroup ps = putBpsStr
 	where
 		putBpsStr = putStrLn $ show (hdrSeconds (fst (head ps))) ++ ": " ++ show bps
 		bps = sum $ map ((preambleEtc +) . toInteger . hdrWireLength . fst) ps
 
+printZeroSecs :: Word32 -> Word32 -> IO ()
+printZeroSecs 0 _ = return ()
+printZeroSecs a b = mapM_ (\s -> putStrLn (show s ++ ": 0")) [(a + 1) .. (b - 1)]
+
+printPacketGroups :: MonadIO m => Conduit [Packet] m [Packet]
+printPacketGroups = evalStateC 0 $ awaitForever $ \thisPktGrp -> do
+	lastTS <- get
+	let thisTS = hdrSeconds . fst $ head thisPktGrp
+	put thisTS
+	liftIO $ printZeroSecs lastTS thisTS
+	liftIO $ printPacketGroup thisPktGrp
+	yield thisPktGrp
+
 printBps :: Conduit Packet IO Packet
-printBps = CL.groupBy isSameSecond =$= CL.concatMapM printAndReturnPkts
+printBps = CL.groupBy isSameSecond =$= printPacketGroups =$= CL.concat
 	where isSameSecond (h1, _) (h2, _) = hdrSeconds h1 == hdrSeconds h2
 
 sumSink :: Sink Packet IO Integer
